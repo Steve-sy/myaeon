9a58d1bfc12d4e1b6f36ad600c25ac31afb54dff
Steve R <mostafa922@gmail.com>
2020-04-13 03:37:58 +0300

    Update cryptonote_config.h

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -166,11 +166,11 @@ namespace config
   uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 0xB2;                // starts with "Wm"
   uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 0x2733;   // starts with "Wz"
   uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 0x06B8;           // starts with "Xn"
-  uint16_t const P2P_DEFAULT_PORT = 11180;
-  uint16_t const RPC_DEFAULT_PORT = 11181;
-  uint16_t const ZMQ_RPC_DEFAULT_PORT = 11182;
+  uint16_t const P2P_DEFAULT_PORT = 15180;
+  uint16_t const RPC_DEFAULT_PORT = 16181;
+  uint16_t const ZMQ_RPC_DEFAULT_PORT = 17182;
   boost::uuids::uuid const NETWORK_ID = { {
-      0x32 ,0x32, 0xF3, 0x91 , 0x81, 0x18 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x10
+      0x32 ,0x32, 0xF3, 0x31 , 0x81, 0x14 , 0x51, 0x61, 0x67, 0x31, 0x00, 0x92, 0x76, 0xA8, 0xA7, 0x20
     } }; // Bender's nightmare
   std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121012bf2d282da90cee9c7a28c16e81418101ee28607d9e50f706594ee144a453b68";
   uint32_t const GENESIS_NONCE = 10000;
@@ -184,7 +184,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 21181;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 21182;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x32 ,0x32, 0xF3, 0x91 , 0x81, 0x18 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
+        0x38 ,0x72, 0xF9, 0x91 , 0x61, 0x19 , 0x41, 0x41, 0x17, 0x31, 0x00, 0x32, 0x16, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210102711ad6274bcf3fa49cf5a99ec02fd3bbfef2dc64fc388df55e7ed8dc310a6f";
     uint32_t const GENESIS_NONCE = 10001;
@@ -199,7 +199,7 @@ namespace config
     uint16_t const RPC_DEFAULT_PORT = 31181;
     uint16_t const ZMQ_RPC_DEFAULT_PORT = 31182;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x32 ,0x32, 0xF3, 0x91 , 0x81, 0x18 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x12
+        0x32 ,0x39, 0xF8, 0x21 , 0x11, 0x18 , 0x41, 0x61, 0x17, 0x31, 0x70, 0x92, 0x16, 0xA1, 0xA1, 0x12
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff0302c54039027b245ae1de97344b5fadd4e3cd01351a0be40579851f565eb885204321015c1d22ecd66bdbf49c80bedf15c72040d059f80a312a85c696841ea8ee53ca34";
     uint32_t const GENESIS_NONCE = 10002;
